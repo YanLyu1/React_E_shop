c66e723d0f17ace6147918540d07a35780abeae6
LaDaDiDaDi <lyu.yan1@husky.neu.edu>
2021-04-28 17:39:07 -0400

    creating sign in components

---
+++ e-shop/src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../../components/custom-button/custom-button.componet'
+import './sign-in.styles.scss'
+
+export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email:'',
+            password:''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.setState({email: '', password: ''})
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target;
+        this.setState({[name]:value})
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+                
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        name='email' 
+                        type='email' 
+                        value={this.state.email} 
+                        handleChange={this.handleChange} 
+                        label='email' 
+                        required/>
+                    {/* <label>Email</label> */}
+                    <FormInput 
+                        name='password' 
+                        type='passowrd' 
+                        value={this.state.password} 
+                        handleChange={this.handleChange} 
+                        label='password' 
+                        required/>
+                    {/* <label>Password</label> */}
+
+                    <CustomButton type='submit'>Submit Form</CustomButton>
+                    {/* <input type='submit' value='Submit Form'/> */}
+                </form>
+            </div>
+        )
+    }
+}
---
+++ e-shop/src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './sign-in-sign-up.styles.scss'
+import Sign from '../../components/sign-in/sign-in.component'
+
+export default function SignInAndSignUp() {
+    return (
+        <div className='sign-in-and-sign-up'>
+            <Sign/>
+        </div>
+    )
+}
