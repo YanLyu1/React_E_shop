fb35af9d2367a7d7d7865cf5e8a502f189fb83e9
LaDaDiDaDi <lyu.yan1@husky.neu.edu>
2021-04-29 00:24:42 -0400

    sign up with email and password

--- e-shop/src/App.js
+++ e-shop/src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-        });
+        }); 
       } else {
         this.setState({currentUser: userAuth});
       }
