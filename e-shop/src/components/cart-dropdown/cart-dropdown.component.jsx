da7bc25d3130fb8b07c689f925733996ab17e163
LaDaDiDaDi <lyu.yan1@husky.neu.edu>
2021-04-30 14:28:33 -0400

    display cart items in shopping list

--- e-shop/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ e-shop/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import CartItem from '../cart-item/cart-item.component'
+
 import CustomButton from '../custom-button/custom-button.componet'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = () => {
+const CartDropdown = ({cartItem}) => {
     return (
         <div className='cart-dropdown'>
             <div className='cart-items'/>
+                {
+                    cartItem.map(ci => (
+                        <CartItem key={ci.id} item={ci}/>
+                    ))
+                }
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 }
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItem}}) => ({
+    cartItem
+})
+export default connect(mapStateToProps)(CartDropdown);
